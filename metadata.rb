bae3e77f6da649c02dcf4658d5ff5120d667697f
Nicholas J <nicholas.a.johns5@gmail.com>
2014-06-28 10:41:48 -0400

    Enable site

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nicholas.a.johns5@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures redstalker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 
 depends          'apt'
 depends          'git'
